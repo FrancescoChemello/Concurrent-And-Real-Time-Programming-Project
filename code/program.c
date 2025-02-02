a51912c042c86640f237e34db2d643ef305278cc
Francesco Chemello <francesco.chemello.1@studenti.unipd.it>
2025-02-02 18:04:41 +0100

    Implemented a shared buffer system to handle the acquisition and saving of frames from a video camera, using semaphores for synchronization between producer and consumer processes.

--- code/program.c
+++ code/program.c
@@ -9,20 +9,72 @@
 */
 
 #include <stdio.h>
-#include <string.h>
-#include <linux/videodev2.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/videodev2.h>    // for v4l2
+#include <sys/ioctl.h>          // for input/output control
+#include <semaphore.h>          // to define semaphores
+#include <sys/mman.h>           // for memory map
+#include <unistd.h>
 
 #define DEVICE "/dev/video0"
-#define MAX_FORMAT 100
+#define BUFFER_SIZE 10
 #define CHECK_IOCTL_STATUS(message)
+#define SHARED_MEMORY_NAME "/shared_buffer"     // give a name to the shared memory -> identify the area
+
+struct BufferData {
+    int head;                   // head of the buffer
+    int tail;                   // tail of the buffer
+    int frame_size;             // size of the frame
+    sem_t mutex;                // semaphore to protect the shared buffer
+    sem_t data_avail;           // semaphore to signal that the buffer is empty
+    sem_t room_avail;            // semaphore to signal that the buffer is full
+    unsigned char buffer[];     // buffer to store the frames (in the end!)
+};
 
 char * usage_string = "Usage: %s <format> <height> <width> <framerate> <timeout>\n";
+struct BufferData * sharedBuf;
+
+static void frame_consumer(){
+    unsigned char frame [sharedBuf->frame_size];
+    while(1){
+        sem_wait(&sharedBuf->data_avail);
+        // enter critical section
+        sem_wait(&sharedBuf->mutex);
+        //collect the frame from the buffer
+        memcpy(frame, sharedBuf->buffer[sharedBuf->head], sizeof(sharedBuf->frame_size));
+        sharedBuf->head = (sharedBuf->head + 1) % BUFFER_SIZE;      // circular array
+        sem_post(&sharedBuf->room_avail);
+        // exit critical section
+        sem_post(&sharedBuf->mutex);
+
+        // TODO: save the frame in the disk ...
+
+    }
+}
+
+static void frame_producer(){
+    unsigned char frame [sharedBuf->frame_size];
+    while(1){
+
+        // TODO: get the frame from the videocamera ...
+
+        sem_wait(&sharedBuf->room_avail);
+        // enter critical section
+        sem_wait(&sharedBuf->mutex);
+        memcpy(sharedBuf->buffer[sharedBuf->tail], frame, sizeof(sharedBuf->frame_size));
+        sharedBuf->tail = (sharedBuf->tail + 1) % BUFFER_SIZE;
+        sem_post(&sharedBuf->data_avail);
+        // exit critical section
+        sem_post(&sharedBuf->mutex);
+    }
+}
 
 int main(int argc, char **argv){
-    int fd, status, idx;
+    int fd, status, memsh;
+
+    int frame_size, height, width;
     
     struct v4l2_capability cap;
     struct v4l2_format fmt;
@@ -105,6 +157,65 @@ int main(int argc, char **argv){
         streamparm.parm.capture.timeperframe.denominator,
         streamparm.parm.capture.timeperframe.numerator);
 
+    // Check the timeout value
+    if (atoi(argv[5]) < 0) {
+        printf("Timeout must be a positive value\n");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Timeout: %d\n", atoi(argv[5]));
+    }
+    
+    // 3. Shared buffer creation with MMap 
+
+    // Dim
+    height = fmt.fmt.pix.height;
+    width = fmt.fmt.pix.width;
+
+    if(fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV){
+        frame_size = height * width * 2;    // 2 bytes for each pixel
+    } else if(fmt.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG){
+        frame_size = height * width * 3;    // 3 bytes for each pixel
+    }
+
+    printf("Frame Size: %d\n", frame_size);
+
+    // Definition of the buffer
+    int total_size = sizeof(struct BufferData) + (BUFFER_SIZE * frame_size);
+
+    memsh = shm_open(SHARED_MEMORY_NAME, O_CREAT | O_RDWR, 0666);
+
+    // Check if I create the shared memory
+    if(memsh == -1){
+        perror("Shared memory creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if I set the correct size of memory (= total_size)
+    if (ftruncate(memsh, total_size) == -1) {
+        perror("Errore ftruncate");
+        exit(EXIT_FAILURE);
+    }
+
+    sharedBuf = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_SHARED, memsh, 0);
+
+    if(sharedBuf == MAP_FAILED){
+        perror("Mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    sharedBuf->head = 0;
+    sharedBuf->tail = 0;
+    sharedBuf->frame_size = frame_size;
+    sem_init(&sharedBuf->mutex, 1, 1);
+    sem_init(&sharedBuf->data_avail, 1, 0);
+    sem_init(&sharedBuf->room_avail, 1, BUFFER_SIZE);
+
+    printf("Memoria condivisa creata con frame_size = %d bytes\n", frame_size);
+
+
+
+    munmap(sharedBuf, total_size);
+    shm_unlink(SHARED_MEMORY_NAME);
     
     return 0;
 }
